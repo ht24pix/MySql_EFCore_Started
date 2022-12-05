10354eeba957da13aabd3664fbb0717aaed65420
ht24pix <huutri1912@gmail.com>
2022-12-05 20:01:09 +0700

    Add project files.

---
+++ LibraryContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+
+namespace MySql_EFCore_Started
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=root;password=");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using MySql_EFCore_Started;
+
+Console.WriteLine("Hello, World!");
+
+InsertData();
+PrintData();
+
+
+ static void InsertData()
+    {
+    using (var context = new LibraryContext())
+    {
+        // Creates the database if not exists
+        context.Database.EnsureCreated();
+
+        // Adds a publisher
+        var publisher = new Publisher
+        {
+            Name = "Mariner Books"
+        };
+        context.Publisher.Add(publisher);
+
+        // Adds some books
+        context.Book.Add(new Book
+        {
+            ISBN = "978-0544003415",
+            Title = "The Lord of the Rings",
+            Author = "J.R.R. Tolkien",
+            Language = "English",
+            Pages = 1216,
+            Publisher = publisher
+        });
+        context.Book.Add(new Book
+        {
+            ISBN = "978-0547247762",
+            Title = "The Sealed Letter",
+            Author = "Emma Donoghue",
+            Language = "English",
+            Pages = 416,
+            Publisher = publisher
+        });
+
+        // Saves changes
+        context.SaveChanges();
+    }
+}
+
+ static void PrintData()
+{
+    // Gets and prints all books in database
+    using (var context = new LibraryContext())
+    {
+        var books = context.Book
+          .Include(p => p.Publisher);
+        foreach (var book in books)
+        {
+            var data = new System.Text.StringBuilder();
+            data.AppendLine($"ISBN: {book.ISBN}");
+            data.AppendLine($"Title: {book.Title}");
+            data.AppendLine($"Publisher: {book.Publisher.Name}");
+            Console.WriteLine(data.ToString());
+        }
+    }
+}
